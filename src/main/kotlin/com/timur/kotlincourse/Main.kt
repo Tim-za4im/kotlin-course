11973d17bd0addeb26764718d92ef097d2a0f989
Timur <bory1914@gmail.com>
2024-09-20 00:57:36 +0500

    Lessons 5

--- src/main/kotlin/com/timur/kotlincourse/Main.kt
+++ src/main/kotlin/com/timur/kotlincourse/Main.kt
@@ -3,7 +3,7 @@ package com.timur.com.timur.kotlincourse
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-    //val standartCoffient : Double = 0.5
+
    soundIntensity ( startIntesity = null, standartCoffient )
     soundIntensity (startIntesity = 6.3, standartCoffient )
 
