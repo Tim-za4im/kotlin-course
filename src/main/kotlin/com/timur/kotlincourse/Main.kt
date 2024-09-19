07778f7f19350e0b306ea234430781d2080815e4
Timur <bory1914@gmail.com>
2024-09-20 01:43:43 +0500

    Lessons 5

--- src/main/kotlin/com/timur/kotlincourse/Main.kt
+++ src/main/kotlin/com/timur/kotlincourse/Main.kt
@@ -3,12 +3,12 @@ package com.timur.com.timur.kotlincourse
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-
-   soundIntensity ( startIntesity = null, standartCoffient )
-    soundIntensity (startIntesity = 4.3, standartCoffient )
+    val standartCoffient : Double = 0.5 //ide выдает ошибку в 7 и 8 строке Unresolved reference, поэтому обьявил переменную заранее
+    soundIntensity ( startIntesity = null, standartCoffient )
+    soundIntensity (startIntesity = 12.7, standartCoffient )
 
 }
-val standartCoffient : Double = 0.5
+
 
   fun soundIntensity ( startIntesity: Double? , standartCoffient:Double=0.5 ) {
       val  intensity : Double = (startIntesity ?: 0.0) * standartCoffient
