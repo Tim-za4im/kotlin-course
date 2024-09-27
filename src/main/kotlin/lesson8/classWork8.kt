d03170832b5cdd3117fb84b0f06007fff83af952
Timur <bory1914@gmail.com>
2024-09-27 23:16:31 +0500

    Lessons 7

---
+++ src/main/kotlin/lesson8/classWork8.kt
@@ -0,0 +1,93 @@
+package lesson8
+
+//val simpleString = "Это простая строка"
+//
+//val firstName = "Иван"
+//val lastName = "Иванов"
+//val fullName = "$firstName $lastName"
+//
+//val age = 30
+//val greeting = "Привет! Меня зовут $fullName, и мне $age лет."
+//
+//
+//val person = Person("Алексей", 25)
+//val introduction = "Меня зовут $person.name, и мне ${person.age} лет."
+//
+//val details = "Здесь находятся ${getDetails()}"
+//
+//val x = 10
+//val y = 20
+//val resultString = "Результат сложения x и y равен ${x + y}"
+//println(resultString)
+
+//al subString = originalString.substring(7)
+//val subString2 = originalString.substring(3, 6)
+//val replacedString = originalString.replace("fun", "awesome")
+//val words = originalString.split(" ")
+
+//al subString = originalString.substring(7)
+//val subString2 = originalString.substring(3, 6)
+//val replacedString = originalString.replace("fun", "awesome")
+//val words = originalString.split(" ")
+//
+//Александр Молокович 22:01
+//val length = "Hello".length
+//val length = "Hello".length
+//val upper = "hello".uppercase()
+//val lower = "HELLO".lowercase()
+//val trimmed = "  hello  ".trim()
+//val trimmed = "  hello  ".trim()
+//val starts = "Kotlin".startsWith("Kot")
+//
+//Александр Молокович  кому  Все 22:06
+//val ends = "Kotlin".endsWith("lin")
+//val contains = "Hello".contains("ell")
+//val empty = "".isNullOrEmpty()
+//val blank = "  ".isNullOrBlank()val repeat = "ab".repeat(3)
+//val letter = originalString[5]
+//val indexOfChar = "Kotlin".indexOf('t')
+//val indexOfWord = "Kotlin is the best language".indexOf("best")val backReverse = "niltoK".reversed()
+//val multiLineString = """
+//    Первая строка
+//    Вторая строка
+//    Третья строка
+//""".trimIndent()val name = "Алексей"
+//val city = "Москва"
+//val age = 32
+//val friendsCount = 1052
+//val rating = 4.948
+//val balance = 2534.75856
+//val text = """
+//   Имя: %s
+//   Город: %s
+//   Возраст: %d
+//   Количество друзей: %,d
+//   Рейтинг пользователя: %.1f
+//   Баланс счета: $%,.2f
+//""".trimIndent()
+//println(text.format(name, city, age, friendsCount, rating, balance))
+
+
+//Правила Проверки и Преобразования:
+//Если фраза начинается с "ошибка":
+//Преобразование: Замените "ошибка" на "небольшое недоразумение".
+//Если фраза заканчивается на "важно":
+//Преобразование: Добавьте в конец фразы "…но не критично".
+//Если фраза содержит слово "проблема":
+//Преобразование: Замените "проблема" на "неожиданность".
+//Если индекс слова "срочно" находится в промежутке от 0 до 10:
+//Преобразование: Замените "срочно" на "когда-нибудь".
+//Если строка пустая:
+//Преобразование: Верните "Кажется, кто-то забыл что-то сказать".
+//Примеры Тестовых Фраз:
+//"Ошибка в системе вызвала панику."
+//"Для завершения проекта важно."
+//"Обнаружена проблема с сетью."
+//"Срочно нужно обновить данные!
+// "Kotlin: The Fun Way to Learn Programming"
+fun main() {
+//    val mumb = 123-456-789
+//    val replaced = mumb.replace{123456,'x'}
+//    println (mumb
+//    )
+}
