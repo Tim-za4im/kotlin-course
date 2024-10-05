7e248f980baaa66585a9b926f471da06405f2a14
Timur <bory1914@gmail.com>
2024-10-06 04:39:11 +0500

    Lessons9 Happy Birthday

---
+++ src/main/kotlin/Lesson10/classwork10.kt
@@ -0,0 +1,39 @@
+package Lesson10
+
+fun main() {
+//    val numbers = arrayOf (1,2,3,4,5)
+//    val doubles : DoubleArray= doubleArrayOf(1.1, 2.2, 3.3)
+//val emtyArray = Array(5){""}
+//    val emtyNullArray = arrayOfNulls<Int>(5)
+//    val readOnlyList : List<String> = listOf("a", "b" , "c")
+//    val mutablist : MutableList <String> = mutableListOf("a", "b", "c")
+//val numbears : Set<Int> = setOf(1,2,3,4,5,5)// уникальные значение
+//    val mutableSetNumbers : MutableSet <Int> = mutableSetOf(1,2,3,4,5)
+//val set = setOf("K", "o","t", "l" ,"i" ,"n" )
+//    for (letters in set){
+//        println("| $letters|")
+//    }
+    //println(numbers [2])
+//val list = listOf( 32 , 53,1, -24)
+//    for (index in list.indices){
+//        if (index == list.lastIndex){
+//        println(list[index] + list[0])}
+//        else {
+//            println(list[index] + list[1])
+//        }
+//    }
+//var index =list.lastIndex
+//    while (index >= 0){
+//        println("${[list[index--]]}")
+//    }
+//    val examle = Array(size = 10) { 0 }
+//    val arrayDouble = doubleArrayOf(1.0, 2.0, 3.0, 4.0, 5.0)
+
+    val array = arrayOf(1, 2, 3)
+    val barray = arrayOf(4, 5, 6)
+    val emtyArray = Array(size = 3) {0}
+    for (i in emtyArray.indices){
+        emtyArray[i] = array[i] + barray [i]
+    }
+    println(emtyArray.toList())
+}
